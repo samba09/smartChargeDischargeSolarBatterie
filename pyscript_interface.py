6b036bfd7c8ef7533856650d53f656a13330fda1
Gerhard Waidelich <gwaidelich@cinemo.com>
2024-12-15 18:28:57 +0100

    fix: task executer needs a variable as param

--- pyscript_interface.py
+++ pyscript_interface.py
@@ -5,16 +5,15 @@ if "/config/pyscript_modules" not in sys.path:
 
 from downloadData import downloadData
 from readData import getBatteryActions
-from mytest import myTestFunc
 
+temp_data = "/config/temp_data"
 
 @service
 def debug_test_call(action=None, id=None):
-    myTestFunc()
     log.info(f"hello_world called {action} {id}")
     
-    task.executor(downloadData,"/config/temp_data")
-    charge_discharge = task.executor(getBatteryActions, "/config/temp_data")
+    task.executor(downloadData,temp_data)
+    charge_discharge = task.executor(getBatteryActions,temp_data)
     do_charge, dont_discharge = charge_discharge
     log.info(f"do charge: {do_charge} and dont discharge: {dont_discharge}")
 
@@ -32,12 +31,12 @@ def debug_test_call(action=None, id=None):
 @time_trigger("cron(0 */3 * * *)")  # Run every 3 hours
 def call_download_data():
     log.info("Calling external DownloadData from via pyscript")
-    task.executor(downloadData,"/config/temp_data")
+    task.executor(downloadData, temp_data)
 
 
 @time_trigger("cron(1/15 * * * *)")  # Start at minute 1 and repeat every 15 minutes
 def call_calculate_data():
-    charge_discharge = task.executor(getBatteryActions, "/config/temp_data")
+    charge_discharge = task.executor(getBatteryActions, temp_data)
     do_charge, dont_discharge = charge_discharge
     log.info(f"do charge: {do_charge} and dont discharge: {dont_discharge}")
 
